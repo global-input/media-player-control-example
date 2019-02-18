95efbc8c2b4161d26558cfea33e66773b78582a1
Dilshat Hewzulla <hewzulla@gmail.com>
2019-02-18 22:11:19 +0000

    media control app example

---
+++ src/styles.js
@@ -0,0 +1,22 @@
+export const styles={
+    container:{
+        display:"flex",
+        flexDirection:"column",
+        justifyContent:"center",
+        alignItems:"center",
+        windows:"100%",
+        minHeight:window.innerHeight*2/3
+
+    },
+    title:{
+      display:"flex",
+      flexDirection:"row",
+      width:"100%",
+      justifyContent:"flex-start",
+      fontSize:"2vw",
+      color:"#5291CD",
+      marginBottom:"2vw",
+      marginLeft:"20vw"
+    }
+
+};
