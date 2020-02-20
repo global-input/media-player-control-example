7638e0e7cb0d7956a27677cc3a0a8e50ea84232f
Dilshat Hewzulla <hewzulla@gmail.com>
2020-02-20 23:07:30 +0000

    refactored

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './media-player-control';
+import App from './example-app';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
